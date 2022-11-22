0c6ad506617a74f9e296ce476e91a7048efdb6a8
jungchungsub <cndtjq1248@naver.com>
2022-11-22 15:26:04 +0900

    firestore insert하기

--- lib/controller/chat_controller.dart
+++ lib/controller/chat_controller.dart
@@ -1,3 +1,30 @@
 //Provider
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/domain/chat/chat_firestore_repository.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
 
-class ChatController {}
+final chatControllerProvider = Provider((ref) {
+  return ChatController(ref);
+});
+
+class ChatController {
+  Ref _ref;
+  ChatController(this._ref);
+
+  void insert(ChatInsertReqDto dto) async {
+    Future<DocumentReference> futureDoc = _ref.read(chatFirestoreRepositoryProvider).insert(dto);
+
+    futureDoc.then((value) {
+      print("document id : ${value.id}");
+      //여기서는 화면 이동
+      //Navigator.pushNamed(context.Routes.login.path);
+      //Navigator.popAndPushNamed(context, Routes.login.path);
+      //Navigator.pop(context);
+      //Navigator.pushNamedAndRemoveUntil(context, Routes.login.paht, (route) => false);
+    }).onError((error, stackTrace) {
+      print("error : ${error}");
+    });
+  }
+}
--- lib/domain/chat/chat_firestore_repository.dart
+++ lib/domain/chat/chat_firestore_repository.dart
@@ -1,3 +1,21 @@
 // Provider
 
-class ChatFirestoreRepository {}
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_firestore_stream/dto/chat/chat_req_dto.dart';
+
+final chatFirestoreRepositoryProvider = Provider((ref) {
+  return ChatFirestoreRepository();
+});
+
+class ChatFirestoreRepository {
+  final db = FirebaseFirestore.instance;
+
+  Future<DocumentReference> insert(ChatInsertReqDto dto) {
+    //Future<DocumentReference> 문서로 돌려받겠다는 것이다.
+    return db.collection("chat").add(dto.toJson());
+  } //db에 insert하는 행위
+  // db.collection("chat").add(dto.toJson()).then((value) => null).onError((error, stackTrace) => null);
+}
